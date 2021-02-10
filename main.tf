4deb0397fd37003547b8010c4037bb5d2c5d8d4f
Alexander Miehe <alexander.miehe@flaconi.de>
2021-02-10 13:28:38 +0100

    * fmt

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 locals {
   clients = {
-  for client in var.clients : replace(lower(client.name), "/[^a-z0-9]/", "-") => {
-    allowed_oauth_flows                  = lookup(client, "allowed_oauth_flows", null)
-    allowed_oauth_flows_user_pool_client = lookup(client, "allowed_oauth_flows_user_pool_client", null)
-    allowed_oauth_scopes                 = lookup(client, "allowed_oauth_scopes", null)
-    callback_urls                        = lookup(client, "callback_urls", null)
-    default_redirect_uri                 = lookup(client, "default_redirect_uri", null)
-    explicit_auth_flows                  = lookup(client, "explicit_auth_flows", null)
-    generate_secret                      = lookup(client, "generate_secret", null)
-    logout_urls                          = lookup(client, "logout_urls", null)
-    read_attributes                      = lookup(client, "read_attributes", null)
-    refresh_token_validity               = lookup(client, "refresh_token_validity", 30)
-    supported_identity_providers         = lookup(client, "supported_identity_providers", null)
-    prevent_user_existence_errors        = lookup(client, "prevent_user_existence_errors", null)
-    write_attributes                     = lookup(client, "write_attributes", null)
-  }
+    for client in var.clients : replace(lower(client.name), "/[^a-z0-9]/", "-") => {
+      allowed_oauth_flows                  = lookup(client, "allowed_oauth_flows", null)
+      allowed_oauth_flows_user_pool_client = lookup(client, "allowed_oauth_flows_user_pool_client", null)
+      allowed_oauth_scopes                 = lookup(client, "allowed_oauth_scopes", null)
+      callback_urls                        = lookup(client, "callback_urls", null)
+      default_redirect_uri                 = lookup(client, "default_redirect_uri", null)
+      explicit_auth_flows                  = lookup(client, "explicit_auth_flows", null)
+      generate_secret                      = lookup(client, "generate_secret", null)
+      logout_urls                          = lookup(client, "logout_urls", null)
+      read_attributes                      = lookup(client, "read_attributes", null)
+      refresh_token_validity               = lookup(client, "refresh_token_validity", 30)
+      supported_identity_providers         = lookup(client, "supported_identity_providers", null)
+      prevent_user_existence_errors        = lookup(client, "prevent_user_existence_errors", null)
+      write_attributes                     = lookup(client, "write_attributes", null)
+    }
   }
 }
 
